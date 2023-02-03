0495a5b62e4159da80a2df7a72779256c7e68471
mfridsen <120990597+mfridsen@users.noreply.github.com>
2023-02-03 15:04:55 +0100

    Added specs to create_item_tables.sql

--- sql/lillabiblioteket/base_tables/create_item_tables.sql
+++ sql/lillabiblioteket/base_tables/create_item_tables.sql
@@ -1,3 +1,16 @@
+--  Generally, a library stocks a number of copies of each book title and each DVD. Each item has a physical location
+--  and have attributes like: author, ISBN, a classification that is used for “keyword” searches e.g. “database”,
+--  “database systems”, “database management” etc.
+
+--  Book publishers are maintained, but not DVD publishers.
+
+--  Books and DVDs have physically attached barcodes which are used for the identification of items that are
+--  checked out (a barcode scanner will be used). In case the item does not scan then it should be also possible to
+--  enter the barcode for the item manually.
+
+--  All course literature also has a reference copy, that is not allowed to leave the library and therefore not
+--  possible to borrow, in other words reference literature.
+
 --  Different categories of items have different loan periods. Standard books are allowed a loan period of 2 months,
 --  Course literature a period of 1 month, Reference literature cannot be borrowed, DVDs have a loan period of 2 weeks.
 CREATE TABLE `Item_Type` (
@@ -21,13 +34,10 @@ CREATE TABLE `Item` (
 
 CREATE TABLE `Book` (
   `Book_ID` INT PRIMARY KEY,
-  `Number_of_Pages` INT,
   FOREIGN KEY (`Book_ID`) REFERENCES `Item`(`Item_ID`)
 );
 
 CREATE TABLE `DVD` (
   `DVD_ID` INT PRIMARY KEY,
-  `Runtime` TIME,	-- HH:MM:SS
-  `Format` VARCHAR(255),
   FOREIGN KEY (`DVD_ID`) REFERENCES `Item`(`Item_ID`)
 );
