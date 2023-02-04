23c32922a4abf78cb0c89e397a038335cee95579
mfridsen <120990597+mfridsen@users.noreply.github.com>
2023-02-04 15:32:04 +0100

    touched up code

--- src/main/java/dev/groupeighteen/librarydatabasesystem/BookBorrowingDataBaseSystem.java
+++ src/main/java/dev/groupeighteen/librarydatabasesystem/BookBorrowingDataBaseSystem.java
@@ -3,20 +3,13 @@ package dev.groupeighteen.librarydatabasesystem;
 import dev.groupeighteen.librarydatabasesystem.model.DatabaseConnection;
 
 public class BookBorrowingDataBaseSystem {
-    private static final String haxxorPath =
-        "sql/lillabiblioteket/create_lillabiblioteket.sql";
-
     public static void main(String[] args) {
-        //DatabaseConnection.connectToDataBase("jdbc:mysql://localhost:3306/lillabiblioteket",
-          //      "root", "password");
-
         //Connect
         DatabaseConnection.connectToMySQLServer();
 
-        //Trying to create the database while still using the same "script"
-        //DatabaseConnection.executeSQLFile(DatabaseConnection.getConnection(), haxxorPath);
-
         setupDatabase();
+
+        //Should always close connection to the database after use
         DatabaseConnection.closeConnectionToDatabase();
         exit(0);
     }
@@ -26,7 +19,7 @@ public class BookBorrowingDataBaseSystem {
         DatabaseConnection.executeSingleSQLCommand("drop database if exists lillabiblioteket;");
         //Create DB
         DatabaseConnection.executeSingleSQLCommand("create database lillabiblioteket;");
-        //Show DBs
+        //Show DBs in server
         DatabaseConnection.executeQuery("SHOW DATABASES;");
         //Use DB
         DatabaseConnection.executeSingleSQLCommand("use lillabiblioteket");
