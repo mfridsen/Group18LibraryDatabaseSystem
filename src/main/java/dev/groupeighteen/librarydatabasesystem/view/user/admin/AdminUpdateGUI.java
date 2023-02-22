67492de4838ca939150ee499ced08c1a87f01e04
mfridsen <120990597+mfridsen@users.noreply.github.com>
2023-02-22 18:30:44 +0100

    added Admin GUI Classes

---
+++ src/main/java/dev/groupeighteen/librarydatabasesystem/view/user/admin/AdminUpdateGUI.java
@@ -0,0 +1,11 @@
+package dev.groupeighteen.librarydatabasesystem.view.user.admin;
+
+/**
+ * @author Mattias Fridsén
+ * @project Group18LibraryDatabaseSystem
+ * @date 2/22/2023
+ */
+
+public class AdminUpdateGUI {
+    // GUI for CRUD Update operations
+}
